d4e362bac40138c65e7fd7d4bfea2b17a696a9e2
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2026-01-04 16:29:07 -0400

    use pallete colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brasilpecuaria.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
